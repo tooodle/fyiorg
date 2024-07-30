dbae5bbc1ffbd0473c6bda65d4cc20fdb4fa12f8
harsh <harshplad@gmail.com>
2024-07-30 15:55:02 +0530

    added meta for domain

--- app/emosphere/page.jsx
+++ app/emosphere/page.jsx
@@ -1,23 +1,68 @@
-import { MoveRight } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+'use client'
+import React, { useState, useEffect } from "react";
 
 function Emosphere() {
+  const maxCharacters = 500;
+  const [text, setText] = useState("");
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const charactersLeft = maxCharacters - text.length;
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    if (!isConfirmed) {
+      alert("Please confirm your confession by checking the box.");
+      return; // Early exit if confirmation is not checked
+    }
+
+    
+    console.log("Confession submitted:", text);
+    setText(""); // Clear the text after successful submission
+    setIsConfirmed(false); // Reset checkbox state
+  };
+
   return (
     <div className="md:px-96 pt-32 px-5 overflow-x-clip">
-      <p className="text-pink-700 text-6xl font-bold">Releasing Soon</p>
-      <p className="text-pink-600 my-12 font-medium text-lg">
-        We&apos;re excited to introduce Emoshpere, your upcoming haven for emotional
-        well-being and mental health support. Our platform is designed to create
-        a safe, nurturing environment where you can share your feelings, find
-        support, and connect with others who understand. Stay tuned as we
-        prepare to launch a space dedicated to fostering emotional growth and
-        well-being. Emoshpere is coming soon to help you navigate your emotions
-        with empathy and understanding.
+      <p className="text-pink-700 text-6xl font-bold">Confess Now!</p>
+      <p className="text-pink-600 my-4 font-medium text-lg">
+        We at FYI respect your privacy and any confessions made are kept
+        anonymous. So without even a second thought just pour it all out.
       </p>
-      <Link href={'https:www.instagram.com/fyiorg'} className="hidden md:block bg-pink-700 hover:bg-white text-white hover:text-pink-700 border-2 rounded-full p-4 border-pink-700 cursor-pointer ">
-        Until then connect with us on instagram 
-      </Link>
+      <div>
+        <textarea
+          id="comment"
+          maxLength={maxCharacters}
+          value={text}
+          onChange={handleInputChange}
+          placeholder="Enter your confession here"
+          className="w-full border-2 border-pink-600 rounded-md min-h-64 p-4 focus:outline-none"
+        />
+        <p id="characterLeft">{charactersLeft} characters left</p>
+        <div className="flex items-center mt-4">
+          <input
+            type="checkbox"
+            id="confirmCheckbox"
+            checked={isConfirmed}
+            onChange={() => setIsConfirmed(!isConfirmed)}
+          />
+          <label htmlFor="confirmCheckbox" className="ml-2">
+            I confirm that my confession is true and anonymous.
+          </label>
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="disabled:opacity-50 bg-pink-600 text-white py-2 px-4 rounded-md focus:outline-none hover:bg-pink-700 mt-5"
+        disabled={!text || !isConfirmed} // Disable submit button if text is empty or confirmation is not checked
+        onClick={handleSubmit}
+      >
+        Submit Confession
+      </button>
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta  name="simpledcver"  content="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJmeWlvcmcuaW4iLCJleHAiOjE3MjI5MDI0MDB9.G6F63ixjm0nzYlfVhSHcs6jxPuyJquiVSQeOa0invlw"/>
+      </head>
       <body className={inter.className+ ' overflow-x-clip'}>
       <div className="flex flex-col min-h-screen">
       <header className="">
